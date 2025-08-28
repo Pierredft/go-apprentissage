26f7c138c91afc9484d01ddc20ffcd3c9e883e1a
pierre defauquet <pierredefauquet@gmail.com>
2025-08-28 10:51:37 +0200

    mise en place des fichiers dans des dossiers, et création de variable

---
+++ Variable/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nom string = "Pierre"
+	var age int = 25
+	var prix float64 = 19.99
+	var actif bool = true
+
+	ville := "Paris"
+	temperature := 20.5
+
+	fmt.Println("Nom:", nom)
+	fmt.Println("Âge:", age)
+	fmt.Println("Prix:", prix)
+	fmt.Println("Actif:", actif)
+	fmt.Println("Ville:", ville)
+	fmt.Println("Température:", temperature)
+}
